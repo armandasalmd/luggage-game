c1cce75ee5258751bdfc2062750e3a8ff59d543f
Armandas Barkauskas (barkausa) <barkausa@coventry.ac.uk>
2022-04-11 14:54:13 +0100

    Settings UI final improvements

--- src/components/templates/SettingsSecurityTab/SettingsSecurityTab.tsx
+++ src/components/templates/SettingsSecurityTab/SettingsSecurityTab.tsx
@@ -1,7 +1,7 @@
 import { FC, useState } from "react";
 import { useSelector } from "react-redux";
 
-import { Button, Card, Input } from "@components/atoms";
+import { Button, Input } from "@components/atoms";
 import { RootState } from "@redux/store";
 import "./SettingsSecurityTab.scss";
 
@@ -36,62 +36,62 @@ const SettingsSecurityTab: FC = () => {
   return (
     <div className="settingsSecurity">
       <div className="settingsSecurity__changePwd">
-        <Card title="Change password">
-          <div className="space">
-            <Input
-              title="Your current password"
-              required
-              password
-              value={getField("currentPassword")}
-              setValue={setField.bind(null, "currentPassword")}
-              onSubmit={handlePwdChange}
-            />
-            <Input
-              title="New password"
-              required
-              password
-              value={getField("newPassword")}
-              setValue={setField.bind(null, "newPassword")}
-              onSubmit={handlePwdChange}
-            />
-            <Input
-              title="Confirm new password"
-              required
-              password
-              value={getField("confirmPassword")}
-              setValue={setField.bind(null, "confirmPassword")}
-              onSubmit={handlePwdChange}
-            />
-            <Button centerText type="ghost" onClick={handlePwdChange}>
+        <h3>Change password</h3>
+        <div className="space">
+          <Input
+            title="Your current password"
+            required
+            password
+            value={getField("currentPassword")}
+            setValue={setField.bind(null, "currentPassword")}
+            onSubmit={handlePwdChange}
+          />
+          <Input
+            title="New password"
+            required
+            password
+            value={getField("newPassword")}
+            setValue={setField.bind(null, "newPassword")}
+            onSubmit={handlePwdChange}
+          />
+          <Input
+            title="Confirm new password"
+            required
+            password
+            value={getField("confirmPassword")}
+            setValue={setField.bind(null, "confirmPassword")}
+            onSubmit={handlePwdChange}
+          />
+          <div>
+            <Button floatRight centerText type="ghost" onClick={handlePwdChange}>
               Change password
             </Button>
           </div>
-        </Card>
+        </div>
       </div>
       <div className="settingsSecurity__deleteAcc">
-        <Card title="Delete account">
-          <div className="space">
-            <p>
-              Deleting your account will lead to your personal data deletion.
-              Please save any important data as you won't be able to recover it
-              afterwards.
-            </p>
-            <Input
-              setValue={setUsername}
-              value={username}
-              required
-              placeholder={user.username}
-              title="Enter username to confirm"
-            />
-            <Button
-              centerText
-              onClick={handleAccDelete}
-              type={user.username === username ? "danger" : "disabled"}
-            >
-              Delete account
-            </Button>
-          </div>
-        </Card>
+        <h3>Delete your account</h3>
+        <div className="space">
+          <p>
+            Deleting your account will lead to your personal data deletion.
+            Please save any important data as you won't be able to recover it
+            afterwards.
+          </p>
+          <Input
+            setValue={setUsername}
+            value={username}
+            required
+            placeholder={user.username}
+            title="Enter username to confirm"
+          />
+          <Button
+            centerText
+            onClick={handleAccDelete}
+            type={user.username === username ? "danger" : "disabled"}
+          >
+            Delete account
+          </Button>
+        </div>
       </div>
     </div>
   );
